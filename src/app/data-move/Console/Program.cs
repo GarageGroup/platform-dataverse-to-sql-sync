9f1519ab4735a1f90aa7dd9790abd7d3a5725c15
Pavel Moskovoy <p@moskovoi.ru>
2023-09-01 01:09:27 +0400

    Update Program.cs

--- src/app/data-move/Console/Program.cs
+++ src/app/data-move/Console/Program.cs
@@ -7,7 +7,6 @@ namespace GarageGroup.Platform.DataMover;
 static class Program
 {
     static Task Main(string[] args)
-            =>
-            Application.UseDataMoveHandler()
-            .RunConsoleAsync(args);
+        =>
+        Application.UseDataMoveHandler().RunConsoleAsync(args);
 }
