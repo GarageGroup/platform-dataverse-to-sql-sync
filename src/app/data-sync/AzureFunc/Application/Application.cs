ea74068a40f0a2f4d6a1a5502116e739a6d94460
pmosk <pm@ggroupp.ru>
2023-12-18 12:41:34 +0400

    Use HealthCheck

---
+++ src/app/data-sync/AzureFunc/Application/Application.cs
@@ -0,0 +1,20 @@
+﻿using GarageGroup.Infra;
+using PrimeFuncPack;
+
+namespace GarageGroup.Platform.DataverseToSqlSync;
+
+internal static partial class Application
+{
+    private static Dependency<IDataverseApiClient> UseDataverseApi()
+        =>
+        PrimaryHandler.UseStandardSocketsHttpHandler()
+        .UseLogging("DataverseApiClient")
+        .UseTokenCredentialStandard()
+        .UsePollyStandard()
+        .UseDataverseApiClient();
+
+    private static Dependency<ISqlApi> UseSqlApi()
+        =>
+        MicrosoftDbProvider.Configure("SqlDb")
+        .UseSqlApi();
+}
