53626fdb7e6f2678d06d899545ac7e7fe5d0d1ee
pmosk <pm@ggroupp.ru>
2025-02-17 19:29:27 +0200

    Increase dependencies

--- src/service/RuleSet.Get/Func/Internal.YamlRead.Func/YamlReadFunc.cs
+++ src/service/RuleSet.Get/Func/Internal.YamlRead.Func/YamlReadFunc.cs
@@ -6,17 +6,17 @@ namespace GarageGroup.Platform.DataverseToSqlSync;
 
 internal sealed partial class YamlReadFunc : IAsyncFunc<string, ConfigurationYaml>
 {
-    private static readonly IDeserializer YamlDeserializer;
-
     static YamlReadFunc()
-    {
-        YamlDeserializer = new DeserializerBuilder().WithNamingConvention(CamelCaseNamingConvention.Instance).Build();
+        =>
         Instance = new();
-    }
 
     public static YamlReadFunc Instance { get; }
 
     private YamlReadFunc()
     {
     }
+
+    private static IDeserializer BuildYamlDeserializer()
+        =>
+        new DeserializerBuilder().WithNamingConvention(CamelCaseNamingConvention.Instance).Build();
 }
